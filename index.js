0d076fab8cde68f937a811ee2dc63eafaa473e19
Anis1020 <anis.dapunia@gmail.com>
2023-05-04 11:10:02 +0600

    added some data in json file

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const app = express();
 var cors = require("cors");
 const port = process.env.PORT || 5000;
 
+const chefData = require("./data/chefData.json");
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("server is running");
+});
+app.get("/chef", (req, res) => {
+  res.send(chefData);
 });
 
 app.listen(port, () => {
